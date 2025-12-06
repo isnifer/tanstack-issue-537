fcd4166824c6504cf70a49ba6de216784642b6e1
Anton Kuznetsov <isnifer@gmail.com>
2025-12-06 04:25:39 +0400

    Create example

---
+++ src/routes/index.tsx
@@ -0,0 +1,29 @@
+import { createFileRoute } from '@tanstack/react-router'
+import '../App.css'
+
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/issue?number=6185982271467&numeric=723421968459640832&sameAsNumericWithoutQuotes=router" style={{ color: 'white' }}>A link from the internet</a>
+        <br />
+        <br />
+        <br />
+        <div>
+          It should open `/issue?number=6185982271467&numeric=723421968459640832&sameAsNumericWithoutQuotes=router`
+        </div>
+        <br />
+        <br />
+        <br />
+        <div>
+          BUT WILL BE `/issue?number=6185982271467&numeric=<span style={{ color: 'red' }}>"</span>7234219684596408<span style={{ color: 'red' }}>00"</span>&sameAsNumericWithoutQuotes=router`
+        </div>
+      </header>
+    </div>
+  )
+}
---
+++ src/routes/issue.tsx
@@ -0,0 +1,79 @@
+import { createFileRoute } from '@tanstack/react-router'
+import z from 'zod'
+
+const zodSchema = z.object({
+  // real world example: 6185982271467
+  number: z.number().optional(),
+  // real world example: 723421968459640832
+  numeric: z.coerce.string().optional(),
+  // not a number value: router
+  sameAsNumericWithoutQuotes: z.coerce.string().optional(),
+})
+
+export const Route = createFileRoute('/issue')({
+  component: RouteComponent,
+  validateSearch: zodSchema,
+  beforeLoad: () => {
+    const url = new URLSearchParams(window.location.search)
+    
+    const string = url.get('string')
+    const number = url.get('number')
+    const numeric = url.get('numeric')
+    const sameAsNumericWithoutQuotes = url.get('sameAsNumericWithoutQuotes')
+
+    return {
+      searchParamsFromURL: {
+        string,
+        number,
+        numeric,
+        sameAsNumericWithoutQuotes,
+      }
+    }
+  },
+})
+
+function RouteComponent() {
+  const search = Route.useSearch()
+  const { searchParamsFromURL } = Route.useRouteContext()
+
+  const parsed = zodSchema.parse({
+    string: searchParamsFromURL.string,
+    number: Number(searchParamsFromURL.number),
+    numeric: searchParamsFromURL.numeric,
+    sameAsNumericWithoutQuotes: searchParamsFromURL.sameAsNumericWithoutQuotes,
+  })
+
+  return (
+    <pre style={{ padding: '2rem' }}>
+      <div>EXPECT number: {search.number} should be 6185982271467 {search.number === 6185982271467 ? '✅' : '❌'}</div>
+      <br />
+      <div>EXPECT numeric: <span style={{ color: 'red', fontWeight: 'bold' }}>{search.numeric}</span> should be 723421968459640832 {search.numeric === '723421968459640832' ? '✅' : '❌'}</div>
+      <br />
+      <div>EXPECT sameAsNumericWithoutQuotes: {search.sameAsNumericWithoutQuotes} should be 'router' {search.sameAsNumericWithoutQuotes === 'router' ? '✅' : '❌'}</div>
+      <br />
+      <br />
+      MAYBE IT'S A ZOD ISSUE? — No
+      <br />
+      <br />
+      <br />
+      {parsed.numeric === '723421968459640832' ? 
+        <>
+          <div>EXPECT number: {parsed.number} should be 6185982271467 {parsed.number === 6185982271467 ? '✅' : '❌'}</div>
+          <br />
+          <div>EXPECT numeric: <span style={{ color: 'green', fontWeight: 'bold' }}>{parsed.numeric}</span> should be 723421968459640832 {parsed.numeric === '723421968459640832' ? '✅' : '❌'}</div>
+          <br />
+          <div>EXPECT sameAsNumericWithoutQuotes: {parsed.sameAsNumericWithoutQuotes} should be 'router' {parsed.sameAsNumericWithoutQuotes === 'router' ? '✅' : '❌'}</div>
+          <br />
+          <br />
+          <div>NOW REFRESH A PAGE TO SEE WHAT'S PARSED FROM SEARCH STRING</div>
+        </> : 
+        <>
+          <div>EXPECT number: {parsed.number} should be 6185982271467 {parsed.number === 6185982271467 ? '✅' : '❌'}</div>
+          <br />
+          <div>EXPECT numeric: <span style={{ color: 'red', fontWeight: 'bold' }}>{parsed.numeric}</span> should be 723421968459640832 {parsed.numeric === '723421968459640832' ? '✅' : '❌'}</div>
+          <br />
+          <div>EXPECT sameAsNumericWithoutQuotes: {parsed.sameAsNumericWithoutQuotes} should be 'router' {parsed.sameAsNumericWithoutQuotes === 'router' ? '✅' : '❌'}</div>
+        </>}
+    </pre>
+  )
+}
